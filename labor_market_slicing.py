66c58a10d29e174de0dfb757c25d51f971ebc294
Guillermo Martínez <guillelozabala@gmail.com>
2024-07-19 13:17:22 +0200

    state level slicing

--- labor_market_slicing.py
+++ labor_market_slicing.py
@@ -1,13 +1,23 @@
 import pandas as pd
 
 df = pd.read_csv(r'./ladata64County.txt', sep="\t")
+df_states = pd.read_csv(r'./la.data.3.AllStatesS.txt', sep="\t")
 
 unique_years = df['year'].unique()
+unique_years_states = df_states['year'].unique()
 
 for year in unique_years:
     df_year = df[df['year'] == year]
     df_year.to_csv(
         f'C:/Users/guill/Documents/GitHub/lmos_opioids/data/source/labor_market_outcomes/lmos_data_{int(year)}.csv',
+        sep=',',
+        index=False
+    )
+
+for year in unique_years_states:
+    df_states_year = df_states[df_states['year'] == year]
+    df_states_year.to_csv(
+        f'C:/Users/guill/Documents/GitHub/lmos_opioids/data/source/labor_market_outcomes_states/lmos_data_{int(year)}.csv',
         sep=',',
         index=False
     )
